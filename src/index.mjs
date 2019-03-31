bd1940eb677bcaffdc32eb32e7df6cd555b120aa
Oliver Turner <oliver.turner@codedsignal.co.uk>
2019-03-31 23:19:49 +0100

    Experimenting with pages

--- src/index.mjs
+++ src/index.mjs
@@ -2,13 +2,13 @@ class SuperMannerist extends HTMLElement {
   constructor() {
     super();
 
-    const wrapper = document.querySelector('#super-mannerist');
+    const wrapper = document.querySelector("#super-mannerist");
     const clone = document.importNode(wrapper.content, true);
-    
-    const shadow = this.attachShadow({mode: 'open'});
+
+    const shadow = this.attachShadow({ mode: "open" });
     shadow.appendChild(clone);
   }
 }
 
 // Define the new element
-customElements.define('super-mannerist', SuperMannerist); 
+customElements.define("super-mannerist", SuperMannerist);
